4524db935d4590e6ce8906c12ffe7a7fa09f8524
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-11 13:32:05 -0700

    adding and removing file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,43 @@
+import { useMemo, useRef, useState } from "react"
+import { ToastContainer } from "react-toastify"
+import { LANGS } from "./constants"
+
 
 function App() {
 
+  const [file, setFile] = useState(null)
+  const fileInputRef = useRef(null)
+
+  const handleOnUpload = (e) => {
+    const file = e.target.files[0]
+    console.log(e.target.files)
+    console.log(file)
+    setFile(file)
+  }
+
+  const isLargeFile = useMemo(() => {
+    return file && file.size > 5000000;
+  }, [file]);
+
+  const handleRemoveFile = () => {
+    setFile(null)
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ""; 
+    }
+  }
+
   return (
+    <div>
       <h1 className="text-3xl font-extralight text-center">
-        Hello world!
+        Choose file
       </h1>
+      <input ref={fileInputRef} type="file" name="file" id="file" onChange={handleOnUpload}/>
+      <button onClick={handleRemoveFile} className="bg-red-500 text-white p-2 rounded mt-2">
+        {LANGS.REMOVE_FILE}
+      </button>
+      {file ? isLargeFile ? <span>{LANGS.UPLOAD_LARGE}</span> : <span>{LANGS.UPLOAD_SMALL}</span> : null}
+      <ToastContainer />
+    </div>
   )
 }
 
---
+++ client/src/constants.js
@@ -0,0 +1,5 @@
+export const LANGS = {
+    UPLOAD_LARGE: "Will upload with multipart",
+    UPLOAD_SMALL: "Will upload with base64",
+    REMOVE_FILE: "Remove file"
+}
