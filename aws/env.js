e71e2399541050b1b87fa702cab08c640a87dbe2
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-13 09:28:24 -0700

    wip

--- aws/env.js
+++ aws/env.js
@@ -1,7 +1,5 @@
 // creating this file as js, so it wont be conflicts with aws env variables
 const CONSTANTS = {
-    AWS_ACCESS_KEY: "AKIAXEVXYPYTOGCGOLIE",
-    AWS_SECRET_ACCESS_KEY: "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",
     AWS_REGION: "us-east-1",
     AWS_CURRENT_BUCKET: "vladimir-unique-bucket-yaml-file",
     AWS_CURRENT_OBJECT: "60026.pdf"
