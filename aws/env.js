284a0ad15e50baa1295e1f975ba8f05878236665
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-13 20:14:33 -0700

    done

--- aws/env.js
+++ aws/env.js
@@ -3,7 +3,7 @@
 const CONSTANTS = {
     AWS_REGION: "us-east-1",
     AWS_CURRENT_BUCKET: "vladimir-unique-bucket-yaml-file",
-    AWS_CURRENT_OBJECT: "60026.pdf"
+    AWS_CURRENT_OBJECT: "myFile.pdf"
 }
 
 module.exports = CONSTANTS;
