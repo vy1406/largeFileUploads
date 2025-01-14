7f6e06054401b677cce6a91043f4a44a63f867ef
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-13 17:33:47 -0700

    adding separator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,30 @@ import TestConnectionBtnS3 from "./components/TestConnectionS3"
 import AwsUploadS3 from "./forms/AwsForm"
 import AwsPreSignedUrl from "./forms/AwsPreSignedUrl"
 import AwsMultipartForm from "./forms/AwsMultipartForm"
+import Separator from "./components/Separator"
 
 function App() {
 
   return (
     <div className="container mx-auto p-4 flex flex-col items-center gap-6">
-      {/* <MultipleFilesForm />
-      <SingleFileForm /> */}
+      <MultipleFilesForm />
+      <Separator />
+      <SingleFileForm />
+      <Separator />
       <AwsMultipartForm />
+      <Separator />
       <AwsPreSignedUrl />
+      <Separator />
       <AwsUploadS3 />
-      <TestConnectionBtn />
-      <TestConnectionBtnS3 />
+      <Separator />
+      <div className="flex flex-row gap-4">
+        <TestConnectionBtn />
+        <TestConnectionBtnS3 />
+      </div>
       <ToastContainer />
     </div>
   )
 }
 
 export default App
+
---
+++ client/src/components/Separator.jsx
@@ -0,0 +1,7 @@
+const Separator = () => {
+    return (
+        <hr className="my-4 border-t border-black-500 w-full" />
+    );
+}
+
+export default Separator;
